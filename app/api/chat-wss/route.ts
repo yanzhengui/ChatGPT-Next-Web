93e80bf0727d4eedb3a52d349e7417f5496a2895
yanzhengui <kea_iv@qq.com>
2023-04-05 22:42:41 +0800

    1

--- app/api/chat-wss/route.ts
+++ app/api/chat-wss/route.ts
@@ -1,42 +1,84 @@
-const http = require('http')
-const WebSocket = require('ws')
+import { createParser } from 'eventsource-parser';
+import http from 'http'
+import { NextRequest } from 'next/server';
+import WebSocket from 'ws'
 
-export default (req, res) => {
-  const server = http.createServer((req, res) => {
-    res.end('I am connected.')
-  })
+async function createStream(req: NextRequest) {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
 
-  const wss = new WebSocket.Server({ server })
+  let apiKey = process.env.OPENAI_API_KEY;
 
-  wss.on('connection', (ws) => {
-    // 建立 WebSocket 连接后，向客户端发送一条欢迎消息
-    ws.send('Welcome to WebSocket server!')
+  const userApiKey = req.headers.get("token");
+  if (userApiKey) {
+    apiKey = userApiKey;
+    console.log("[Stream] using user api key");
+  }
 
-    // 模拟实时更新数据
-    const intervalId = setInterval(() => {
-      const data = {
-        time: new Date(),
-        message: 'hello world'
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    method: "POST",
+    body: req.body,
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      function onParse(event: any) {
+        if (event.type === "event") {
+          const data = event.data;
+          // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
+          if (data === "[DONE]") {
+            controller.close();
+            return;
+          }
+          try {
+            const json = JSON.parse(data);
+            const text = json.choices[0].delta.content;
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+          } catch (e) {
+            controller.error(e);
+          }
+        }
       }
-      ws.send(JSON.stringify(data))
-    }, 1000)
 
-    // 处理 WebSocket 关闭事件
-    ws.on('close', () => {
-      clearInterval(intervalId)
-    })
-    // 处理各种 WebSocket 消息
-    ws.on('message', (message) => {
-      console.log(`Received message from client: ${message}`)
-      const response = `Server received your message: ${message}`
-      ws.send(response)
-    })
-  })
-
-  server.listen(80, () => {
-    console.log(`WebSocket server is running at ws://localhost:${process.env.PORT}/`)
-  })
-
-  // 防止翻墙API代理
-  res.setHeader('Access-Control-Allow-Origin', '*')
+      const parser = createParser(onParse);
+      for await (const chunk of res.body as any) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+  return stream;
 }
+
+export async function POST() {
+  const server = new WebSocket.Server({ port: 80 });
+  server.on('connection', (socket: WebSocket) => {
+    console.log('connection established');
+  
+    // 在连接建立后，处理客户端发送的消息
+    socket.on('message', (data: WebSocket.Data) => {
+      console.log(`received: ${data}`);
+      
+      // 接收到消息后，在此处编写相应逻辑处理逻辑，并将结果通过 send 方法发送给客户端
+      socket.send('response to client');
+    });
+  
+    // 连接断开时的处理
+    socket.on('close', () => {
+      console.log('connection closed');
+    });
+  });
+
+  server.addListener('listening', () => {
+    console.log('WebSocket server is listening on port 80...');
+  });
+}
+
+export const config = {
+  runtime: "edge",
+};
+
