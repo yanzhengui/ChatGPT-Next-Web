3626481c6e803d5ea1872d91178037f3ec26d2a0
yanzhengui <yanzhengui@longshine.com>
2023-04-11 11:46:55 +0800

    1

--- app/api/poll/route.ts
+++ app/api/poll/route.ts
@@ -1,17 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getCache } from "../cacheUtil";
 
-
-
-export default async function handler(
+export function GET(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { id }: any = req.query;
+  console.log("获取到id："+id);
   try {
     let buffer = getCache(id);
+    console.log("获取到buffer");
     return res.status(200).json(buffer?.toString());
   } catch (error) {
+    console.log("获取缓存失败："+error);
     return res.status(500).json({ message: '获取失败' });
   }
 }
